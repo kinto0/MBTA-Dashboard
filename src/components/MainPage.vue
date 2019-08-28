82a17e8d3149d82e26c2ead0c124fbc719718c11
Kyle Into <kyleatyahoo@gmail.com>
2019-08-28 10:46:23 -0400

    added veautify

---
+++ src/components/MainPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Please select a station</h1>
+
+    <NextTrain direction="Inbound"/>
+    <NextTrain direction="Outbound"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage"
+};
+</script>
